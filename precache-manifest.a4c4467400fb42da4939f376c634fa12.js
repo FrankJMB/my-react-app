b5dfa85666f74f6451de3478ec691c1d7c82ee3e
FrankJMB <frank.beentjes@live.nl>
2019-03-13 15:26:22 +0100

    Updates

---
+++ precache-manifest.a4c4467400fb42da4939f376c634fa12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b28cb091c153ab1cf392",
+    "url": "/my-react-app/static/js/runtime~main.f5ab8794.js"
+  },
+  {
+    "revision": "e11ccbf339db7396c959",
+    "url": "/my-react-app/static/js/main.fde4047e.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/my-react-app/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "e11ccbf339db7396c959",
+    "url": "/my-react-app/static/css/main.ec8be6a2.chunk.css"
+  },
+  {
+    "revision": "750da0d861ec1d9e7cdf69e15576ea4a",
+    "url": "/my-react-app/index.html"
+  }
+];
